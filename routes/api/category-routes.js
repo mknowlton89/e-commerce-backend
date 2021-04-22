6a5ccd6258e153b1d066550e037dd3d8ebfd1cb4
Michael Knowlton <mknowlton89@gmail.com>
2021-04-22 15:44:06 -0400

    Built the first round for categories.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,19 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get('/', async (req, res) => {
+  try {
+    // find all categories
+    // be sure to include its associated Products
+    const categories = await Category.findAll();
+    if (!categories) {
+      res.status(404).json({ message: 'No categories found' });
+      return;
+    }
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
