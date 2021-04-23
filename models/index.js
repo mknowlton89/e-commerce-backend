091a0027bfcf375db9dd87d0ee12a0c4f92bb952
Michael Knowlton <mknowlton89@gmail.com>
2021-04-22 20:46:21 -0400

    Got all but two of the product-route routes finalized. Just working on the last two through joins.

--- models/index.js
+++ models/index.js
@@ -15,10 +15,22 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+as: 'product_tag_connection'
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+as: 'product_tag_connection'
+});
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,13 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tag_connection' }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.response(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tag_connection' }],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that ID"})
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.response(500).json(err);
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
